324ba90eef8b0c187706052cc9075834e69ea6b6
vinisouzapq <mvinicius112015@gmail.com>
2020-06-03 22:12:19 -0300

    Primeira alteração. Feito até o exercicio 4 letra a

---
+++ Exercicio3/src/app/model/pessoas.model.ts
@@ -0,0 +1,9 @@
+export interface Peassoas {
+    id: number;
+    first_name: String;
+    last_name: String;
+    email: String;
+    gender: String;
+    latitude: String;
+    longitude: String;
+}
